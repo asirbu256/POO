3caecf03cfca7a79b45704441c7f82712a99efdb
Sirbu Alexandra <asirbu256@gmail.com>
2025-10-26 14:26:20 +0200

    Farmacy class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include<iostream>
+#include <iostream>
+
+class Farmacie
+{
+public:
+	static std::string CLASS_NAME = "FARMACIE";
+
+	Farmacie() {};
+	virtual ~Farmacie() {};
+
+public:
+	std::string name_;
+	std::string address_;
+}
 
 int main ()
 {
-	std::cout<<"Hello world!"<<std::endl;
+	std::cout<<"Hello world from " << Farmacie::CLASS_NAME <<std::endl;
 	return 0;
 }
